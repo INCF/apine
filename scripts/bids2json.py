54f2ca799ceaeee044d415c6a437c2c8b8c7109b
gkiar <gkiar07@gmail.com>
2017-09-01 11:17:31 -0700

    updated bids2json for more corner cases and multiple runs; generated on all INCF/BIDS-examples datasets

--- scripts/bids2json.py
+++ scripts/bids2json.py
@@ -22,6 +22,7 @@ def craftBIDS(bids_dir):
     bids = BIDSLayout(bids_dir)
     bids_dict = OrderedDict()
 
+    # First, add all the dataset metadata...
     desc = op.join(bids_dir, 'dataset_description.json')
     part = op.join(bids_dir, 'participants.tsv')
     sesh = op.join(bids_dir, 'sessions.tsv')
@@ -29,6 +30,7 @@ def craftBIDS(bids_dir):
     read = op.join(bids_dir, 'README')
     chng = op.join(bids_dir, 'CHANGES')
 
+    # ... provided it exists, of course
     bids_dict['dataset_description'] = json.load(open(desc))
     if op.isfile(part): bids_dict['demographics'] = part
     if op.isfile(sesh): bids_dict['sessions'] = sesh
@@ -36,49 +38,81 @@ def craftBIDS(bids_dir):
     if op.isfile(read): bids_dict['README'] = read
     if op.isfile(chng): bids_dict['CHANGES'] = chng
     
+    # Now, for every participant...
     bids_dict['participants'] = OrderedDict()
     for subid in bids.get_subjects():
+
         part_dict = OrderedDict()
         #TODO: remove once @zorro patches in grabbids
         if not op.isdir(op.join(bids_dir, 'sub-{}'.format(subid))):
             print("sub-{} detected, but no directory found!!".format(subid))
             continue
+
+        # And for every session (imposing "1" if none are explicit)...
         part_dict["sessions"] = OrderedDict()
         nosesh = len(bids.get_sessions()) == 0
         sesh_array = ["1"] if nosesh else bids.get_sessions()
         for sesid in sesh_array:
+
+            # And for every modality...
             sesh_dict = OrderedDict()
             for mod in bids.get_modalities():
                 sesh_dict[mod] = OrderedDict()
+
+                # Get corresponding data
                 if nosesh:
                     data = bids.get(subject=subid, modality=mod, extensions="nii|nii.gz")
                 else:
                     data = bids.get(subject=subid, session=sesid, modality=mod, extensions="nii|nii.gz")
 
+                # Now, for every piece of data for this participant, session, and modality...
                 for dat in data:
-                    if mod == 'anat':
-                        ty = dat.type
-                        sesh_dict[mod][ty] = OrderedDict()
-                        sesh_dict[mod][ty]["filename"] = dat.filename
-                        if bids.get_metadata(dat.filename) is not None:
-                            sesh_dict[mod]["metadata"] = bids.get_metadata(dat.filename)
-                    elif mod == "dwi":
-                        if bids.get_metadata(dat.filename) is not None:
-                            sesh_dict[mod]["metadata"] = bids.get_metadata(dat.filename)
-                        sesh_dict[mod]["filename"] = dat.filename
-                        sesh_dict[mod]["bval"] = bids.get_bval(dat.filename)
-                        sesh_dict[mod]["bvec"] = bids.get_bvec(dat.filename)
-                    elif mod == "func":
-                        task = dat.task
-                        sesh_dict[mod][task] = OrderedDict()
-                        if bids.get_metadata(dat.filename) is not None:
-                            sesh_dict[mod][task]["metadata"] = bids.get_metadata(dat.filename)
-                        sesh_dict[mod][task]["metadata"] = bids.get_metadata(dat.filename)
-                        sesh_dict[mod][task]["filename"] = dat.filename
-                        if bids.get_events(dat.filename) is not None:
-                            sesh_dict[mod][task]["events"] = bids.get_events(dat.filename)
+
+                    # Get the run (imposing "1" if none are explicit)
+                    try:
+                        run = dat.run
+                    except AttributeError:
+                        run = 1
+
+                    # Get the file-shorthand (fish) which will be a dictionary key
+                    if mod == 'func':
+                        fish = dat.task
                     else:
-                        print("modality {} not found?".format(mod))
+                        fish = dat.type
+
+                    # Create the runs task/type and run dictionaries if they don't exist
+                    try:
+                        sesh_dict[mod][fish]
+                    except KeyError:
+                        sesh_dict[mod][fish] = OrderedDict()
+
+                    try:
+                        sesh_dict[mod][fish]["runs"]
+                    except KeyError:
+                        sesh_dict[mod][fish]["runs"] = OrderedDict()
+
+                    # Create an entry for this run
+                    sesh_dict[mod][fish]["runs"][run] = OrderedDict()
+
+                    # Add the filename
+                    sesh_dict[mod][fish]["runs"][run]["filename"] = dat.filename
+
+                    # If metadata is available, add it
+                    if bids.get_metadata(dat.filename) is not None:
+                        sesh_dict[mod][fish]["runs"][run]["metadata"] = bids.get_metadata(dat.filename)
+
+                    # If events are available, add them
+                    if mod == "func" and bids.get_events(dat.filename) is not None:
+                        sesh_dict[mod][fish]["runs"][run]["events"] = bids.get_events(dat.filename)
+
+                    # If diffusion, add bvals and bvecs
+                    if mod == "dwi":
+                        sesh_dict[mod][fish]["runs"][run]["bval"] = bids.get_bval(dat.filename)
+                        sesh_dict[mod][fish]["runs"][run]["bvec"] = bids.get_bvec(dat.filename)
+
+            # Flatten DWI trees, since they received an extra level above
+            if "dwi" in bids.get_modalities():
+                sesh_dict["dwi"] = sesh_dict["dwi"]["dwi"]
 
             part_dict["sessions"][sesid] = sesh_dict
         bids_dict['participants'][subid] = part_dict
