e298d683e2b92e6d2146758dfb408f34f7b07311
gkiar <gkiar07@gmail.com>
2018-02-07 23:26:05 -0500

    updated controller to return dataset metadata

--- swagger/python-flask-server-generated/swagger_server/controllers/dataset_controller.py
+++ swagger/python-flask-server-generated/swagger_server/controllers/dataset_controller.py
@@ -8,8 +8,9 @@ from swagger_server.models.dataset import Dataset  # noqa: E501
 from swagger_server.models.datasets import Datasets  # noqa: E501
 from swagger_server import util
 
-json_dir = os.path.join(os.path.dirname(__file__), '..', 'jsons')
-contents = os.listdir(json_dir)
+
+dset_json_dir = os.path.join(os.path.dirname(__file__), '..', 'jsons')
+dset_jsons = [fl for fl in os.listdir(dset_json_dir) if fl.endswith('.json')]
 
 
 def get_dataset(datasetID):  # noqa: E501
@@ -22,7 +23,31 @@ def get_dataset(datasetID):  # noqa: E501
 
     :rtype: List[Dataset]
     """
-    return "({}) These ARE the IDs you're looking for: ".format(contents) + ", ".join(datasetID)
+    datasets = [dset for di in datasetID for dset in dset_jsons if di in dset]
+    contents = {}
+    for dset in datasets:
+        thing = json.load(open(os.path.join(dset_json_dir, dset)))
+        with open(os.path.join(dset_json_dir, dset)) as fhandle:
+            dset_contents = json.load(fhandle)
+            modalities = []
+            participants = []
+            sessions = []
+            for dset_obj in dset_contents:
+                modalities   += [dset_obj.get('modality')] \
+                                if dset_obj.get('modality') is not None \
+                                else []
+                participants += [dset_obj.get('participant')] \
+                                if dset_obj.get('participant') is not None \
+                                else []
+                sessions     += [dset_obj.get('session')] \
+                                if dset_obj.get('session') is not None \
+                                else []
+
+        dset = dset.strip('.json')
+        contents[dset] = { 'modalities':   list(set(modalities)),
+                           'participants': list(set(participants)),
+                           'sessions':     list(set(sessions)) }
+    return contents
 
 
 def list_datasets():  # noqa: E501
