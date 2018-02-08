ac7c536e8131b42dbea7add32e0fd545434fece9
gkiar <gkiar07@gmail.com>
2018-02-08 12:26:53 -0500

    added modalities querying to datasets

--- swagger/python-flask-server-generated/swagger_server/controllers/dataset_controller.py
+++ swagger/python-flask-server-generated/swagger_server/controllers/dataset_controller.py
@@ -13,7 +13,7 @@ dset_json_dir = os.path.join(os.path.dirname(__file__), '..', 'jsons')
 dset_jsons = [fl for fl in os.listdir(dset_json_dir) if fl.endswith('.json')]
 
 
-def get_dataset(datasetID):  # noqa: E501
+def get_dataset(datasetID, modality=['']):  # noqa: E501
     """Find a dataset
 
     Will return information about the specified dataset # noqa: E501
@@ -29,24 +29,30 @@ def get_dataset(datasetID):  # noqa: E501
         thing = json.load(open(os.path.join(dset_json_dir, dset)))
         with open(os.path.join(dset_json_dir, dset)) as fhandle:
             dset_contents = json.load(fhandle)
-            modalities = []
-            participants = []
-            sessions = []
+            dset_modalities = []
+            dset_participants = []
+            dset_sessions = []
             for dset_obj in dset_contents:
-                modalities   += [dset_obj.get('modality')] \
-                                if dset_obj.get('modality') is not None \
-                                else []
-                participants += [dset_obj.get('participant')] \
-                                if dset_obj.get('participant') is not None \
-                                else []
-                sessions     += [dset_obj.get('session')] \
-                                if dset_obj.get('session') is not None \
-                                else []
+                dset_modalities   += [dset_obj.get('modality')] \
+                                     if dset_obj.get('modality') is not None \
+                                     else []
+                dset_participants += [dset_obj.get('participant')] \
+                                     if dset_obj.get('participant') is not None \
+                                     else []
+                dset_sessions     += [dset_obj.get('session')] \
+                                     if dset_obj.get('session') is not None \
+                                     else []
+        print(modality, len(modality))
+        if modality != ['']:
+            valid_modality = all(mod in dset_modalities for mod in modality)
+            print(valid_modality)
+            if not valid_modality:
+                continue;
 
         dset = dset.strip('.json')
-        contents[dset] = { 'modalities':   list(set(modalities)),
-                           'participants': list(set(participants)),
-                           'sessions':     list(set(sessions)) }
+        contents[dset] = { 'modalities':   list(set(dset_modalities)),
+                           'num_participants': len(set(dset_participants)),
+                           'sessions':     list(set(dset_sessions)) }
     return contents
 
 
